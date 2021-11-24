7b04dd265911e18dae0e118fa4d6d49801d7bbff
rutiz <rutiz@wix.com>
2021-11-24 14:34:31 +0200

    after styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Note from './components/Note';
+import './App.css';
+
+export interface Inote {
+  id: number;
+  value: string;
+  y: number;
+  x: number;
+}
 
 export default function App() {
   const [open, SetOpen] = useState<boolean>(false);
-  const [notes, setNotes] = useState<any>([]);
+  const [notes, setNotes] = useState<Inote[]>(() => {
+    const localNotes = window.localStorage.getItem('localNotes');
+    return localNotes !== null ? JSON.parse(localNotes) : [];
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('localNotes', JSON.stringify(notes));
+  }, [notes]);
+
+  const newNote: Inote = {
+    id: Math.random(),
+    value: '',
+    x: 50,
+    y: 50,
+  };
+
+  const updateNotes = (note: Inote) => {
+    const updatingNotes = [...notes];
+    const index = updatingNotes.findIndex((noteA) => {
+      return noteA.id === note.id;
+    });
+    updatingNotes[index] = note;
+    setNotes(updatingNotes);
+  };
+
+  const deleteNote = (id: number) => {
+    const deleteNotes = [...notes];
+    const index = deleteNotes.findIndex((note) => {
+      return note.id === id;
+    });
+    deleteNotes.splice(index, 1);
+    setNotes(deleteNotes);
+  };
 
   return (
     <>
-      <div>
-        <button onClick={() => SetOpen(!open)}>+</button>
+      <div className='main-container'>
         {open && (
-          <>
+          <div className='button-list'>
             <button onClick={() => {}}>Embedd a link</button>
             <button
               onClick={() => {
-                setNotes([...notes, 1]);
+                setNotes([...notes, newNote]);
               }}
             >
               Add a note
             </button>
             <button onClick={() => {}}>Surprise me</button>
-          </>
+          </div>
         )}
+        <button className='main-btn' onClick={() => SetOpen(!open)}>
+          +
+        </button>
       </div>
-      {notes.map((note: any, index: number) => {
-        return <Note key={index} />;
+      {notes.map((note: Inote, index: number) => {
+        return (
+          <Note
+            key={index}
+            id={note.id}
+            value={note.value}
+            x={note.x}
+            y={note.y}
+            updateNotes={updateNotes}
+            deleteNote={deleteNote}
+          />
+        );
       })}
     </>
   );
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
+import { Inote } from '../App';
 
-export default function Note() {
-  const [noteText, setNoteText] = useState<string>('');
-  const [x, setX] = useState<number>();
-  const [y, setY] = useState<number>();
+interface INoteComponent {
+  id: number;
+  value: string;
+  y: number;
+  x: number;
+  updateNotes: (note: Inote) => void;
+  deleteNote: (id: number) => void;
+}
+
+export default function Note({
+  id,
+  value,
+  x,
+  y,
+  updateNotes,
+  deleteNote,
+}: INoteComponent) {
+  const [noteText, setNoteText] = useState<string>(value);
+  const [xCor, setX] = useState<number>(x);
+  const [yCor, setY] = useState<number>(y);
 
   return (
-    <textarea
-      draggable
-      onDragEnd={(e) => {
-        console.log(e.pageX, e.pageY);
-        setX(e.pageX);
-        setY(e.pageY);
-      }}
-      value={noteText}
-      onChange={(e) => {
-        setNoteText(e.target.value);
-      }}
-      style={{ position: 'absolute', left: x, top: y }}
-    />
+    <div
+      className='note-container'
+      style={{ position: 'absolute', left: xCor, top: yCor }}
+    >
+      <button className='note-close' onClick={() => deleteNote(id)}>
+        X
+      </button>
+      <textarea
+        rows={8}
+        className='note'
+        draggable
+        onDragEnd={(e) => {
+          console.log(e.pageX, e.pageY);
+          setX(e.pageX);
+          setY(e.pageY);
+          updateNotes({ id, value: noteText, x: e.pageX, y: e.pageY });
+        }}
+        value={noteText}
+        onChange={(e) => {
+          setNoteText(e.target.value);
+          updateNotes({ id, value: e.target.value, x: xCor, y: yCor });
+        }}
+      />
+    </div>
   );
 }
