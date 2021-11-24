d8ad7f839eca21d0eb85fb219ad41b231ae46fc0
rutiz <rutiz@wix.com>
2021-11-24 13:00:27 +0200

    dragging

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 
 export default function Note() {
   const [noteText, setNoteText] = useState<string>('');
-  const [x, setX] = useState();
-  const [y, setY] = useState();
+  const [x, setX] = useState<number>();
+  const [y, setY] = useState<number>();
 
   return (
     <textarea
       draggable
       onDragEnd={(e) => {
         console.log(e.pageX, e.pageY);
+        setX(e.pageX);
+        setY(e.pageY);
       }}
       value={noteText}
-      onChange={(e) => setNoteText(e.target.value)}
+      onChange={(e) => {
+        setNoteText(e.target.value);
+      }}
       style={{ position: 'absolute', left: x, top: y }}
     />
   );
